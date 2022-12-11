9e4079b4b68da82e8c9a2d62a7ae55e073334059
David Gipson <david.gipson@utexas.edu>
2022-12-11 17:59:56 -0600

    Extract shared examples for tests

--- spec/advent_of_code22_spec.rb
+++ spec/advent_of_code22_spec.rb
@@ -1,75 +1,46 @@
 # frozen_string_literal: true
 
+RSpec.shared_examples "a day's solution" do |input_dirname, part_1_ex_exp, part_1_exp, part_2_ex_exp, part_2_exp|
+  let (:data_dir) { Pathname.new(__FILE__) + "../data/" }
+  let (:input_dir) { data_dir + input_dirname }
+  let (:example) { IO.read(input_dir + "example") }
+  let (:input) { IO.read(input_dir + "test") }
+
+  describe "#part_1" do
+    it "follows the example" do
+      expect(described_class.part_1 example).to eq(part_1_ex_exp)
+    end
+
+    it "solves part 1" do
+      expect(described_class.part_1 input).to eq(part_1_exp)
+    end
+  end
+
+  describe "#part_2" do
+    it "follows the example" do
+      expect(described_class.part_2 example).to eq(part_2_ex_exp)
+    end
+
+    it "solves part 2" do
+      expect(described_class.part_2 input).to eq(part_2_exp)
+    end
+  end
+end
+
 RSpec.describe AdventOfCode22 do
   it "has a version number" do
-    expect(AdventOfCode22::VERSION).not_to be nil
+    expect(described_class::VERSION).not_to be nil
   end
 
-  let(:data_dir) { Pathname.new(__FILE__) + "../data/" }
-
-  describe "Day1" do
-    let (:input_dir) { data_dir + "day1" }
-    let (:example) { IO.read(input_dir + "example") }
-    let (:input) { IO.read(input_dir + "test") }
-
-    it "follows the part 1 example" do
-      expect(AdventOfCode22::Day1.part_1 example).to eq(24000)
-    end
-
-    it "solves part 1" do
-      expect(AdventOfCode22::Day1.part_1 input).to eq(70720)
-    end
-
-    it "follows the part 2 example" do
-      expect(AdventOfCode22::Day1.part_2 example).to eq(45000)
-    end
-
-    it "solves part 2" do
-      expect(AdventOfCode22::Day1.part_2 input).to eq(207148)
-    end
-  end
-  
-  describe "Day2" do
-    let (:input_dir) { data_dir + "day2" }
-    let (:example) { IO.read(input_dir + "example") }
-    let (:input) { IO.read(input_dir + "test") }
-
-    it "follows the part 1 example" do
-      expect(AdventOfCode22::Day2.part_1 example).to eq(15)
-    end
-
-    it "solves part 1" do
-      expect(AdventOfCode22::Day2.part_1 input).to eq(13682)
-    end
-
-    it "follows the part 2 example" do
-      expect(AdventOfCode22::Day2.part_2 example).to eq(12)
-    end
-
-    it "solves part 2" do
-      expect(AdventOfCode22::Day2.part_2 input).to eq(12881)
-    end
+  describe described_class::Day1 do
+    it_behaves_like "a day's solution", "day1", 24000, 70720, 45000, 207148
   end
 
-  describe "Day3" do
-    let (:input_dir) { data_dir + "day3" }
-    let (:example) { IO.read(input_dir + "example") }
-    let (:input) { IO.read(input_dir + "test") }
+  describe described_class::Day2 do
+    it_behaves_like "a day's solution", "day2", 15, 13682, 12, 12881
+  end
 
-    it "follows the part 1 example" do
-      expect(AdventOfCode22::Day3.part_1 example).to eq(157)
-    end
-
-    it "solves part 1" do
-      expect(AdventOfCode22::Day3.part_1 input).to eq(8185)
-    end
-
-    it "follows the part 2 example" do
-      expect(AdventOfCode22::Day3.part_2 example).to eq(70)
-    end
-
-    it "solves part 2" do
-      expect(AdventOfCode22::Day3.part_2 input).to eq(2817)
-    end
+  describe described_class::Day3 do
+    it_behaves_like "a day's solution", "day3", 157, 8185, 70, 2817
   end
 end
